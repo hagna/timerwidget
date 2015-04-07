1553111d8557211fc3445b19efe665e8e8b2a7d1
hagna <natester@gmail.com>
2015-04-07 14:07:36 -0600

    improve example with more keyboard

--- example/gauge.go
+++ example/gauge.go
@@ -10,12 +10,96 @@ import ("github.com/gizak/termui"
 	"github.com/hagna/timerwidget"
 	"time"
 	"flag"
+	"log"
+	"strconv"
+	_ "net/http/pprof"
+	"net/http"
 )
 
 var fuse = flag.Duration("t", 10 * time.Second, "timeout")
 
+type stateargs struct {
+	evt chan tm.Event
+	widget *timerwidget.Widget
+	pause bool
+	nums string
+}
+
+type stateFn func(*stateargs) stateFn
+
+func nums(s *stateargs) stateFn {
+	w := s.widget
+	select {
+		case e := <-s.evt:
+			if e.Type == tm.EventKey {
+				switch e.Ch {
+					case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+						s.nums += string(e.Ch)
+						return nums
+				}
+				switch e.Key {
+					case tm.KeyEnter:
+						n, err := strconv.Atoi(s.nums)
+						if err == nil {
+							w.Len = time.Second * time.Duration(n)
+							s.nums = ""
+							w.Border.Label = w.Len.String()
+						} else {
+							w.Border.Label = err.Error()
+						}
+						return state1
+				}
+		}
+	}
+	return nums
+}
+
+
+func state1(s *stateargs) stateFn {
+	w := s.widget
+	select {
+		case e := <-s.evt:
+			if e.Type == tm.EventKey {
+				switch e.Ch {
+					case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+						s.nums += string(e.Ch)
+						return nums	
+					case 'q':
+						return nil
+					case 'p':
+						s.pause = ! s.pause
+						if s.pause == false {
+							w.Retime()
+						}
+					case 'r':
+						w.Rewind()
+					case 'z':
+						w.Increasing = ! w.Increasing
+					case '+':
+						w.Len += time.Second * 1
+						w.Border.Label = w.Len.String()
+					case '-':
+						w.Len -= time.Second * 1
+						w.Border.Label =  w.Len.String()
+				}
+			}
+
+		default:
+			termui.Render(w)
+			if ! s.pause {
+				w.Update()
+			}
+			time.Sleep(time.Second / 4)
+		}
+	return state1
+}
+
 func main() {
 	flag.Parse()
+go func() {
+	log.Println(http.ListenAndServe(":6060", nil))
+}()
+
 	err := termui.Init()
 	if err != nil {
 		panic(err)
@@ -33,33 +117,12 @@ func main() {
 		}
 	}()
 	pause := false
-FOR:
-	for {
-		select {
-		case e := <-evt:
-			if e.Type == tm.EventKey {
-				switch e.Ch {
-					case 'q':
-						break FOR
-					case 'p':
-						pause = ! pause
-						if pause == false {
-							w.Retime()
-						}
-					case 'r':
-						w.Rewind()
-					case 'z':
-						w.Decreasing = ! w.Decreasing
-				}
-			}
-
-		default:
-			termui.Render(w)
-			if ! pause {
-				w.Update()
-			}
-			time.Sleep(time.Second / 2)
-		}
+	s := stateargs{pause:pause, evt: evt, widget: w}
+	newstate := state1(&s)
+	for newstate != nil {
+		
+		newstate = newstate(&s)
+				
 	}
 
 }
--- timer.go
+++ timer.go
@@ -10,25 +10,25 @@ import (
 type Widget struct {
 	*termui.Gauge
 	starttime time.Time
-	len       time.Duration
+	Len       time.Duration
 	elapsed   time.Duration
-	Decreasing bool
+	Increasing bool
 }
 
 
 func (w0 *Widget) setStart() {
-	if w0.Decreasing {
-		w0.Percent = 100
-	} else {
+	if w0.Increasing {
 		w0.Percent = 0
+	} else {
+		w0.Percent = 100
 	}
 }
 
 func (w0 *Widget) setEnd() {
-	if w0.Decreasing {
-		w0.Percent = 0
-	} else {
+	if w0.Increasing {
 		w0.Percent = 100
+	} else {
+		w0.Percent = 0
 	}
 }
 
@@ -39,7 +39,7 @@ func New(t time.Duration, w, h int, label string) *Widget {
 	w0.Width = w
 	w0.Height = h
 	w0.Border.Label = label
-	w0.len = t
+	w0.Len = t
 	w0.starttime = time.Now()
 	return w0
 }
@@ -49,16 +49,16 @@ func Round(f float64) float64 {
 }
 
 func (w *Widget) Update() {
-	if w.elapsed >= w.len {
+	if w.elapsed >= w.Len {
 		w.setEnd()
 		return
 	}
 
 	t := time.Since(w.starttime)
 	w.elapsed += t
-	p := Round(float64(w.elapsed*100)/float64(w.len))
-	//log.Println(w.Border.Label, "w.len is", w.len, "w.elapsed is", w.elapsed, "percent is", p)
-	if w.Decreasing {
+	p := Round(float64(w.elapsed*100)/float64(w.Len))
+	//log.Println(w.Border.Label, "w.Len is", w.Len, "w.elapsed is", w.elapsed, "percent is", p)
+	if w.Increasing == false {
 		w.Percent = int(math.Max(0, 100 - p))
 	}  else {
 		w.Percent = int(math.Min(100, p))
